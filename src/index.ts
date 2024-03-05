0eb79947560b8b2cdc3ffab4d9a98dac78bb4f1a
Jamal Shah <jshah@fluxworkstudios.com>
2024-03-04 22:26:29 -0500

    feat: add hono

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.info('Hello World!');
+import { config } from 'dotenv';
+config();
+
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+
+const port = parseInt(process.env.PORT) || 3000;
+
+const app = new Hono();
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!');
+});
+
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
